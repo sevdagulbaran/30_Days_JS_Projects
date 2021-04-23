c72aea19709c8834acc40d76e6f0a9cb52245b7d
Sevda <74216005+sevdagulbaran@users.noreply.github.com>
2021-04-23 23:29:37 +0300

    Delete Day-08 directory

--- Day-08/canvas.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.querySelector('#draw');
-const ctx = canvas.getContext('2d');
-
-canvas.width = window.innerWidth;
-canvas.height= window.innerHeight;
-
-ctx.strokeStyle = '#BADA55';
-
