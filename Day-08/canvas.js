1afe89d503683aa46a5ec5d1e7bad49341fbb7de
sevdagulbaran <sevdagulbaran@gmail.com>
2021-04-24 05:15:33 +0300

    canvas

--- Day-08/canvas.js
+++ Day-08/canvas.js
@@ -1,8 +1,45 @@
 const canvas = document.querySelector('#draw');
-const ctx = canvas.getContext('2d');
-
+const ctx = canvas.getContext('2d');//canvas element
 canvas.width = window.innerWidth;
-canvas.height= window.innerHeight;
-
+canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 50;
+ctx.globalCompositeOperation = 'lighten';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0; //start to edit coloring
+let direction = true;
+
+function draw(e){
+  if(!isDrawing) return; // stop the func when not moused down
+  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`; // coloring - mother-effing hsl
+  //do the drawing
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY); //start from
+  ctx.lineTo(e.offsetX, e.offsetY); // go to
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // ES6 destructuring an array
+  hue++; // changes color
+if(ctx.lineWidth >=100 || ctx.lineWidth <=1){
+  direction =!direction;
+}
+if(direction){
+  ctx.lineWidth++;
+} else {
+  ctx.lineWidth--;
+}
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mousemove' , draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
 
